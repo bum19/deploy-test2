8cfe13e1e231d184bf60c27db2db251610e99ece
bum19 <1996joon@gmail.com>
2023-01-15 23:17:40 +0900

    자동배포다시시작

--- server/src/main/java/com/example/deploytest/sample/SampleController.java
+++ server/src/main/java/com/example/deploytest/sample/SampleController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
     @GetMapping
     public String helloWorld() {
-        return "몸살때매 죽겠다 어우.. 근데 배포안되니까 짜증..";
+        return "몸살때매 죽겠다 어우.. 근데 배포안되니까 짜증..마지막인사";
     }
 }
